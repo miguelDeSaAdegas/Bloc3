d72c1ca84e226045c18b3a517b3c203b372ebcce
Miguel De Sa Adegas <miguel.desaadegas@student.vinci.be>
2023-10-18 12:51:50 +0200

    setup semaine 4 + test generic dao

---
+++ .Net/semaine4/SchoolApp/SchoolApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SchoolApp.Models;
+using SchoolApp.Repository;
+using System.Collections;
+
+SchoolContext context = new SchoolContext();
+
+BaseRepositorySQL<Student> repoStudent = new BaseRepositorySQL<Student>(context);
+
+Student eleve = new Student();
+eleve.Name = "de sa adegas";
+eleve.YearResult = 1;
+eleve.Firstname = "miguel";
+
+
+repoStudent.Insert(eleve);
+
+IList<Student> list = repoStudent.GetAll();
+
+foreach (Student ele in list)
+{
+    Console.WriteLine(ele.Name);
+}
